34e841707b3fbeebd60bbb0d5c7662010d2cc9ec
Samuel Guerrero <samuelg510@gmail.com>
2025-09-07 12:03:14 -0500

    Diseño de la pantalla principal (parte 1)

--- app/src/main/java/com/fasodi/ecommerceappmvvm/MainActivity.kt
+++ app/src/main/java/com/fasodi/ecommerceappmvvm/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.fasodi.ecommerceappmvvm.ui.theme.KotlinEcommerceMVVMTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,14 +34,41 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KotlinEcommerceMVVMTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LoginContent()
             }
         }
+    }
+}
+
+@Composable
+fun LoginContent(){
+    Box(modifier = Modifier.fillMaxSize()){
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "Imagen de fondo",
+            contentScale = ContentScale.Crop,
+            colorFilter = ColorFilter.colorMatrix(ColorMatrix().apply { setToScale(0.5f,0.5f,0.5f,1f) })
+            )
+        Column(
+            modifier=Modifier.fillMaxWidth().padding(top = 50.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Image(
+                modifier = Modifier.height(100.dp).width(100.dp),
+                painter = painterResource(R.drawable.shopping_cart_blue),
+                contentDescription = "Logo",
+
+            )
+            Text(
+                modifier = Modifier.padding(top = 7.dp),
+                text="ECOMMERCE APP",
+                fontWeight = FontWeight.Bold,
+                color=Color.White,
+                fontSize=20.sp
+                )
+        }
+
     }
 }
 
@@ -38,7 +80,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     KotlinEcommerceMVVMTheme {
