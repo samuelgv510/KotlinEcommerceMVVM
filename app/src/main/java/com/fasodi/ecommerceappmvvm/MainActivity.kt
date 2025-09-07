9815a607ee0ea33e9f2560c0c7c1edda2295f0d2
Samuel Guerrero <samuelg510@gmail.com>
2025-09-07 12:31:27 -0500

    (App) Diseño de la pantalla principal (parte 2)

--- app/src/main/java/com/fasodi/ecommerceappmvvm/MainActivity.kt
+++ app/src/main/java/com/fasodi/ecommerceappmvvm/MainActivity.kt
@@ -5,15 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -65,8 +72,56 @@ fun LoginContent(){
                 text="ECOMMERCE APP",
                 fontWeight = FontWeight.Bold,
                 color=Color.White,
-                fontSize=20.sp
+                fontSize=22.sp
                 )
+            Spacer(modifier = Modifier.weight(1f))
+            Card(
+                modifier = Modifier.fillMaxWidth().height(330.dp),
+                shape = RoundedCornerShape(
+                topEnd = 40.dp,
+                topStart = 40.dp
+            )
+               ){
+                Column(
+                    modifier = Modifier.padding(20.dp)
+                ){
+                    Text(
+                        modifier = Modifier.padding(bottom = 20.dp),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        color = Color.Black,
+                        text = "INGRESAR")
+                    TextField(
+                        modifier=Modifier.fillMaxWidth(),
+                        value="",
+                        onValueChange={},
+                        label = {
+                            Text(text = "Correo electronico")
+                        }
+                    )
+                    TextField(
+                        modifier=Modifier.fillMaxWidth(),
+                        value="",
+                        onValueChange={},
+                        label = {
+                            Text(text = "Contraseña")
+                        }
+                    )
+                    Button(
+                        modifier=Modifier.fillMaxWidth(),
+                        onClick = {}) {
+                        Text(text="LOGIN")
+                    }
+                    Row(
+                        modifier=Modifier.fillMaxWidth().padding(vertical = 10.dp),
+                        Arrangement.Center
+                    ){
+                        Text(text = "No tienes cuenta?")
+                        Text(text = "Registrate",
+                            color=Color.Blue)
+                    }
+                }
+            }
         }
 
     }
